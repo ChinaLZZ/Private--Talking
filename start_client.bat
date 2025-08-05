42280a8d5537d3a1e5f46546e2ed92760e5ae9b3
ChinaLZZ <lzzwwee@163.com>
2025-08-05 17:46:07 +0800

    Delete start_client.bat

--- start_client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ==========================================
-echo Private Talking Chat Client
-echo ==========================================
-echo.
-echo 正在启动客户端...
-echo.
-python start_client.py
-pause 
