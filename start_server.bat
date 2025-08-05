58b51b5cd1a806f0c82a73494613baaaf9d39056
ChinaLZZ <lzzwwee@163.com>
2025-08-05 17:46:29 +0800

    Delete start_server.bat

--- start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ==========================================
-echo Private Talking Chat Server
-echo ==========================================
-echo.
-echo 正在启动服务器...
-echo.
-python start_server.py
-pause 
